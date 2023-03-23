9829edd81e647550bb158b32ab63238895f75300
Jake Spurlock <whyisjake@gmail.com>
2023-03-23 07:30:40 -0700

    Add the missing file

---
+++ components/_weatherTypes.js
@@ -0,0 +1,140 @@
+// These are a bunch of weather types that are used in the weather API.
+// They are used in the WeatherDetails component.
+export const weatherTypes = {
+  BlowingDust: {
+    icon: "WIND",
+    name: "Blowing Dust",
+  },
+  Clear: {
+    icon: "CLEAR_DAY",
+    name: "Clear",
+  },
+  Cloudy: {
+    icon: "CLOUDY",
+    name: "Cloudy",
+  },
+  Foggy: {
+    icon: "FOG",
+    name: "Foggy",
+  },
+  Haze: {
+    icon: "FOG",
+    name: "Haze",
+  },
+  MostlyClear: {
+    icon: "PARTLY_CLOUDY_DAY",
+    name: "Mostly Clear",
+  },
+  MostlyCloudy: {
+    icon: "PARTLY_CLOUDY_DAY",
+    name: "Mostly Cloudy",
+  },
+  PartlyCloudy: {
+    icon: "PARTLY_CLOUDY_DAY",
+    name: "Partly Cloudy",
+  },
+  Smoky: {
+    icon: "FOG",
+    name: "Smoky",
+  },
+  Breezy: {
+    icon: "WIND",
+    name: "Breezy",
+  },
+  Windy: {
+    icon: "WIND",
+    name: "Windy",
+  },
+  Drizzle: {
+    icon: "RAIN",
+    name: "Drizzle",
+  },
+  heavyRain: {
+    icon: "RAIN",
+    name: "Heavy Rain",
+  },
+  isolatedThunderstorms: {
+    icon: "RAIN",
+    name: "Isolated Thunderstorms",
+  },
+  Rain: {
+    icon: "RAIN",
+    name: "Rain",
+  },
+  sunShowers: {
+    icon: "RAIN",
+    name: "Sun Showers",
+  },
+  scatteredThunderstorms: {
+    icon: "RAIN",
+    name: "Scattered Thunderstorms",
+  },
+  strongStorms: {
+    icon: "RAIN",
+    name: "Strong Storms",
+  },
+  thunderstorms: {
+    icon: "RAIN",
+    name: "Thunderstorms",
+  },
+  frigid: {
+    icon: "SLEET",
+    name: "Frigid",
+  },
+  hail: {
+    icon: "SLEET",
+    name: "Hail",
+  },
+  hot: {
+    icon: "CLEAR_DAY",
+    name: "Hot",
+  },
+  flurries: {
+    icon: "SNOW",
+    name: "Flurries",
+  },
+  sleet: {
+    icon: "SLEET",
+    name: "Sleet",
+  },
+  snow: {
+    icon: "SNOW",
+    name: "Snow",
+  },
+  sunFlurries: {
+    icon: "RAIN",
+    name: "Sun Flurries",
+  },
+  wintryMix: {
+    icon: "SNOW",
+    name: "Wintry Mix",
+  },
+  blizzard: {
+    icon: "SNOW",
+    name: "Blizzard",
+  },
+  blowingSnow: {
+    icon: "SNOW",
+    name: "Blowing Snow",
+  },
+  freezingDrizzle: {
+    icon: "RAIN",
+    name: "Freezing Drizzle",
+  },
+  freezingRain: {
+    icon: "RAIN",
+    name: "Freezing Rain",
+  },
+  heavySnow: {
+    icon: "SNOW",
+    name: "Heavy Snow",
+  },
+  hurricane: {
+    icon: "RAIN",
+    name: "Hurricane",
+  },
+  TropicalStorm: {
+    icon: "RAIN",
+    name: "Tropical Storm",
+  },
+};
