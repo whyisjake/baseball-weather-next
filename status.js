ba119e0b4be4ef2fce12ba37b9a7fc3ce02aaed4
Jake Spurlock <whyisjake@gmail.com>
2023-05-02 16:25:56 -0700

    Let's add the date too.

--- status.js
+++ status.js
@@ -1,7 +1,7 @@
 // export baseball field status.
 module.exports = {
   isClosed: true,
-  updated: "4:30 PM",
+  updated: "5/2/2023 - 4:30 PM",
   message: "Fields are closed for the day.",
   shortMessage: "Closed",
 };
