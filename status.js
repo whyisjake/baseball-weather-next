7e995ab8f1c6a3fe08a01152d59a3fcf26513bcc
Jake Spurlock <whyisjake@gmail.com>
2023-05-04 08:42:31 -0700

    Reopen things

--- status.js
+++ status.js
@@ -1,7 +1,7 @@
 // export baseball field status.
 module.exports = {
-  isClosed: true,
-  updated: "5/3/2023 - 4:00 PM",
-  message: "All fields are a game time decision.",
-  shortMessage: "Game time decision",
+  isClosed: false,
+  updated: "",
+  message: "",
+  shortMessage: "",
 };
