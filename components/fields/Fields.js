7473eaeb3301eaddd95d0798efa1cbbb87fb88f9
Jake Spurlock <whyisjake@gmail.com>
2024-03-01 14:29:40 -0800

    Get the field details to work

--- components/fields/Fields.js
+++ components/fields/Fields.js
@@ -1,9 +1,7 @@
-// Export all of the info files from the fields directory.
-// export the fields/yve/info.js file as yve.
-
-// This needs to be refactored. I am getting this error:
-// Error: ES Modules may not assign module.exports or exports.*, Use ESM export syntax, instead: ./components/fields/Fields.js
-
 import yve from "./yve/info";
 
-export { yve };
+const Fields = {
+  yve,
+};
+
+export default Fields;
