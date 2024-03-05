4874e1be782c70524439cfef3b7e78a0f6619052
Jake Spurlock <whyisjake@gmail.com>
2024-03-05 13:07:25 -0800

    All fields are open

--- status.js
+++ status.js
@@ -1,9 +1,7 @@
 // export baseball field status.
 module.exports = {
-  isClosed: true,
-  updated: "Friday, March 1st, 2024",
-  message:
-    "Fields are closed due to weather. Please check back later for updates.",
-  shortMessage:
-    "Fields are closed due to weather. Please check back later for updates.",
+  isClosed: false,
+  updated: "",
+  message: "",
+  shortMessage: "",
 };
