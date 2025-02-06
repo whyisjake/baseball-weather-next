5eac0ec2106a8ea878cd7f3ec1bc06523a2e91fa
Jake Spurlock <whyisjake@gmail.com>
2025-02-06 07:34:46 -0800

    Close the fields

--- status.js
+++ status.js
@@ -1,7 +1,7 @@
 // export baseball field status.
 module.exports = {
-  isClosed: false,
-  updated: "",
-  message: "",
-  shortMessage: "",
+  isClosed: true,
+  updated: "2/6/2025",
+  message: "All fields are closed today",
+  shortMessage: "Closed",
 };
