1d9b63af7efa6bea765cd8ca455efa8bb78534db
Jake Spurlock <whyisjake@gmail.com>
2025-03-13 07:36:14 -0700

    Make the status a little more dynamic.

--- status.js
+++ status.js
@@ -1,7 +1,23 @@
 // export baseball field status.
 module.exports = {
-  isClosed: false,
+  isClosed: true,
   updated: "",
   message: "",
   shortMessage: "",
+  checkFieldStatus: function () {
+    const now = new Date("3/12/25");
+    this.updated = now.toLocaleDateString("en-US");
+
+    if (this.isClosed) {
+      this.message = "The fields are closed.";
+      this.shortMessage = "Closed";
+    } else {
+      this.isClosed = false;
+      this.message = "The fields are open.";
+      this.shortMessage = "Open";
+    }
+  },
 };
+
+// Call the function to set the initial status
+module.exports.checkFieldStatus();
